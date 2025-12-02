b88c7ff810f73ebd29a6384cda47cf2f796db6b2
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-02 19:17:44 +0900

    Update _config.php

--- landing/www.adm/landing_log/_config.php
+++ landing/www.adm/landing_log/_config.php
@@ -1,12 +1,14 @@
 <?php
+//error_reporting(E_ALL); // 모든 에러 표시
+//ini_set('display_errors', 1); // 에러를 브라우저에 표시
+
 $path = '';
 while (!file_exists($path.'_common.php') && realpath($path) !== '/') {$path .= '../';}
 include_once $path.'_common.php';
 
-// 기본 게시판 정보
-$cf_bo_table = 'landing';
+$sub_menu = '750100';
 $bo_table = 'landing_log';
-$board = get_board_db($bo_table);
+$page_title = '랜딩페이지 DB';
 
-// 원본 글 테이블
 $target_table = $g5['write_prefix'] . $bo_table;
+$board = get_board_db($bo_table);
