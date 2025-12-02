3e2f2a087c06b7323313613555f85e298a43c94d
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-02 19:13:55 +0900

    Remove unnecessary comments from _config.php

--- contact/_config.php
+++ contact/_config.php
@@ -2,9 +2,6 @@
 //error_reporting(E_ALL); // 모든 에러 표시
 //ini_set('display_errors', 1); // 에러를 브라우저에 표시
 
-/**
- * wr_sort 유형, wr_tel 전화번호, wr_memo 간단메모 추가
-**/
 $path = '';
 while (!file_exists($path.'_common.php') && realpath($path) !== '/') {$path .= '../';}
 include_once $path.'_common.php';
