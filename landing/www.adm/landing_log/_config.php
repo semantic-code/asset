4ffad80568f9e7f7956bc0912f62bfaa9ac4efe8
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-15 01:49:52 +0900

    Update _config.php

--- landing/www.adm/landing_log/_config.php
+++ landing/www.adm/landing_log/_config.php
@@ -7,10 +7,11 @@ while (!file_exists($path.'_common.php') && realpath($path) !== '/') {$path .= '
 include_once $path.'_common.php';
 
 $sub_menu = '750100';
-$bo_table = 'landing_log';
+$bo_table = $location ?? 'landing_log';
 $cf_bo_table = 'landing';
 $page_title = '랜딩페이지 DB';
 
 $target_table = $g5['write_prefix'] . $bo_table;
 $board = get_board_db($bo_table);
+
 
