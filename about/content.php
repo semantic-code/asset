098e6018d292260322bb386279c489951aa0a5a5
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-16 01:36:44 +0900

    Create content.php

---
+++ about/content.php
@@ -0,0 +1,14 @@
+<?php
+$sub_menu = '100100';
+$page_title = '문의하기';
+$g5['title'] = $page_title;
+
+include_once(G5_PATH.'/head.php');
+?>
+
+<main>
+    /about/content.php
+</main>
+
+<?php
+include_once(G5_PATH.'/tail.php');
