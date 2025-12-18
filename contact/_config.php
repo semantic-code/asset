506b133957e2792258f5f006a539d438653f0037
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-19 00:34:11 +0900

    Add error handling for empty board value
    Add check for empty board value and handle error.

--- contact/_config.php
+++ contact/_config.php
@@ -10,6 +10,9 @@ $sub_menu = '500100';
 $bo_table = $location ?? 'contact';
 $page_title = '문의하기';
 
-$target_table = $g5['write_prefix'] . $bo_table;
 $board = get_board_db($bo_table);
-
+if (!empty($board['bo_table'])) {
+    $target_table = $g5['write_prefix'] . $bo_table;
+} else {
+    die('board  값이 없습니다.');
+}
