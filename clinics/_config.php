54c992950f77ef0ae197fd302fd4022718108e39
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-22 00:53:36 +0900

    Add initial configuration for clinics module

---
+++ clinics/_config.php
@@ -0,0 +1,17 @@
+<?php
+//error_reporting(E_ALL); // 모든 에러 표시
+//ini_set('display_errors', 1); // 에러를 브라우저에 표시
+
+$path = '';
+while (!file_exists($path.'_common.php') && realpath($path) !== '/') {$path .= '../';}
+include_once $path.'_common.php';
+
+$sub_menu = '500300';
+$bo_table = $location ?? 'clinics';
+$page_title = '클리닉';
+
+$target_table = $g5['write_prefix'] . $bo_table;
+$board = get_board_db($bo_table);
+
+//add_stylesheet('<link rel="stylesheet" href="_style.css">', 0);
+//<link rel="stylesheet" href="/adm/notice/style.css">
