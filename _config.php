c24b00dfdf2cc1268ff5234f3834dd636894c8c9
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-12-25 01:13:53 +0900

    Create _config.php

---
+++ _config.php
@@ -0,0 +1,22 @@
+<?php
+//error_reporting(E_ALL); // 모든 에러 표시
+//ini_set('display_errors', 1); // 에러를 브라우저에 표시
+
+$path = '';
+while (!file_exists($path.'_common.php') && realpath($path) !== '/') {$path .= '../';}
+include_once $path.'_common.php';
+
+$sub_menu = '500100';
+$bo_table = $location ?? 'notice';
+$page_title = '공지사항';
+
+$board = get_board_db($bo_table);
+if (!empty($board['bo_table'])) {
+    $target_table = $g5['write_prefix'] . $bo_table;
+} else {
+    die('board 값이 없습니다.');
+}
+
+add_stylesheet('<link rel="stylesheet" href="_style.css">', 0);
+//add_stylesheet('<link rel="stylesheet" href="'. G5_URL .'/'.$bo_table.'/_style.css">', 0);
+//<link rel="stylesheet" href="/adm/notice/style.css">
