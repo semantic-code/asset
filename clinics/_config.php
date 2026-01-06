c9e23a3a984af4d90cbf62755541e8b42962c9cc
semantic-code <73548323+semantic-code@users.noreply.github.com>
2026-01-06 19:22:09 +0900

    Update _config.php

--- clinics/_config.php
+++ clinics/_config.php
@@ -17,7 +17,6 @@ if (!empty($board['bo_table'])) {
 } else {
     die('board 값이 없습니다.');
 }
-$target_table = $g5['write_prefix'] . $bo_table;
 
 //add_stylesheet('<link rel="stylesheet" href="_style.css">', 0);
 //<link rel="stylesheet" href="/adm/notice/style.css">
